984fb1557492320a59bb38e92207840be38c0be3
Sonia Baranova <sofiasapsan@gmail.com>
2024-08-15 19:00:21 +0300

    cleaned code

--- src/main/java/org/test_task/validation/book/author/AuthorName.java
+++ src/main/java/org/test_task/validation/book/author/AuthorName.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = TitleValidator.class)
+@Constraint(validatedBy = AuthorNameValidator.class)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthorName {
